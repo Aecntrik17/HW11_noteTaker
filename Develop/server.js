892c4f86c666a890fedac802843c271b56533cf6
Aaron Sims <aaron@adsimsllc.com>
2020-07-31 10:08:05 -0400

    creating html routes

--- Develop/server.js
+++ Develop/server.js
@@ -15,6 +15,17 @@ app.use(express.json());
 // for all frontend files, search the "public" folder
 app.use(express.static("public"));
 
+// Routes
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+  // when the / is requested use this function to send the html file to the browser
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/notes", function (req, res) {
+  // when the /notes is requested use this function to send the html file to the browser
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
 // Listener
 app.listen(PORT, function () {
   console.log("App listening on PORT" + PORT);
