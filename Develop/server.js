5905ee77bf8c96ab03fa0bbf875ba50727c79888
Aaron Sims <aaron@adsimsllc.com>
2020-07-31 20:42:50 -0400

    fixing delete route method, updating port to allow for heroku connection

--- Develop/server.js
+++ Develop/server.js
@@ -2,12 +2,12 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const notes = require("./db/db.json");
+let notes = require("./db/db.json");
 
 // connecting express, activating the server
 const app = express();
-// setting the local host port for the app to run on
-const PORT = 8080;
+// setting the local host port for the app to run on. but allowing for heroku to choose first
+const PORT = process.env.PORT || 8080;
 // allowing for nested objects
 app.use(express.urlencoded({ extended: true }));
 // when sending data back and forth make sure its formatted in JSON
@@ -27,17 +27,19 @@ app.get("/notes", function (req, res) {
 });
 
 // API routes
+// this route is allowing the user to view the existing note
 app.get("/api/notes", function (req, res) {
   // when the /api/notes is requested use this function to send the data back in json format
   res.json(notes);
 });
-
+// this route is allowinf the user to save a new note to the note array
 app.post("/api/notes", function (req, res) {
   const newNote = req.body;
   // setting conditional to check if the note has an id, and assigns a new accordingly
   if (notes.length === 0) {
     newNote.id = 1;
   } else {
+    // if the note already has an id, then give it a nmber higher than the last existing id
     const newNoteId = notes[notes.length - 1].id + 1;
     newNote.id = newNoteId;
   }
@@ -47,17 +49,21 @@ app.post("/api/notes", function (req, res) {
   notes.push(newNote);
   // writing the string into the json file, in json format
   fs.writeFileSync("./db/db.json", JSON.stringify(notes));
-  res.json(notes);
+  //  sending back to client new db entry
+  res.json(newNote);
 });
 
 // method to remove an id from the notes array
 app.delete("/api/notes/:id", function (req, res) {
   const id = parseInt(req.params.id);
-  var newNotes = notes.filter((note) => {
+  // look for the chosen id and remove it from the array
+  const newNotes = notes.filter((note) => {
     return note.id !== id;
   });
-
+  // rewrite the db with the new notes array
   fs.writeFileSync("./db/db.json", JSON.stringify(newNotes));
+  // set the original notes variable to the new data set
+  notes = newNotes;
   res.json(newNotes);
 });
 
